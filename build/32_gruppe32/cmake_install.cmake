b46848f2acadde65355530bd7404b62b6a6fbc44
Per Simen Reed <persr18@uia.no>
2024-05-20 10:39:08 +0000

    updated build

--- build/32_gruppe32/cmake_install.cmake
+++ build/32_gruppe32/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /workspaces/2024-ikt218-osdev/src/18_gruppe18
+# Install script for directory: /workspaces/2024-ikt218-osdev/src/32_gruppe32
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/workspaces/2024-ikt218-osdev/build/18_gruppe18/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/workspaces/2024-ikt218-osdev/build/32_gruppe32/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
